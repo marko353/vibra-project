6d1a9a5149e28f75ae8f923ff0d33af67b84cdf6
LAPTOP-OFIE2952\Marko <markostojanovic353@gmail.com>
2025-03-24 17:12:29 +0100

    edit deploy errors

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,30 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'scss', 'css'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-      '^.+\\.scss$': 'jest-transform-stub', // Za SCSS fajlove
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    moduleNameMapper: {
-      '\\.scss$': 'identity-obj-proxy', // Mock-uje SCSS fajlove
-      '\\.css$': 'identity-obj-proxy',  // Mock-uje CSS fajlove
-    },
-  };
-  
-  export default config;
-  
-  
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'scss', 'css'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.scss$': 'jest-transform-stub', // Za SCSS fajlove
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.scss$': 'identity-obj-proxy', // Mock-uje SCSS fajlove
+    '\\.css$': 'identity-obj-proxy',  // Mock-uje CSS fajlove
+  },
+  reporters: [
+    'default', // Default reporter
+    [
+      'jest-html-reporter',
+      {
+        pageTitle: 'My Test Report',
+        outputPath: './test-report.html',
+        includeFailureMsg: true, 
+        includeSuiteFailure: true, 
+      },
+    ],
+  ],
+};
+
+export default config;
