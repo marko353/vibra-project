7de3f5365c2e2d49ff61d8e7b2736eee9b8427c8
LAPTOP-OFIE2952\Marko <markostojanovic353@gmail.com>
2025-03-24 21:54:07 +0100

    edit

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000"; 
+const SOCKET_URL = "https://vibra-backend-production-c7bc.up.railway.app"; 
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
